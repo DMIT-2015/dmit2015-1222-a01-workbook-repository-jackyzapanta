dd4d2ddbce302afbba094bea43e9c80494e55a14
jackyzapanta <jackyzapanta@gmail>
2023-01-13 11:53:15 -0700

    test

---
+++ dmit2015-azure-app/src/main/java/dmit2015/model/Circle.java
@@ -0,0 +1,23 @@
+package dmit2015.model;
+
+public class Circle {
+
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle() {
+
+
+    }
+
+    static void main(String[] args){
+
+    }
+}
