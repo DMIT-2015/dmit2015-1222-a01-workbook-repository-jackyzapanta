5e39ea9738e3edf60e2f5170eea559d7c9158085
jackyzapanta <jackyzapanta@gmail>
2023-01-30 17:07:21 -0700

    servlet

--- demo2/src/test/java/dmit2015/ProgrammingLanguageTest.java
+++ demo2/src/test/java/dmit2015/ProgrammingLanguageTest.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.*; //for @Test and Assertions class
 import static org.junit.jupiter.api.Assertions.*; //for assertEquals()
 
 public class ProgrammingLanguageTest {
-    @Test // 2^3 should be 8
-    public void powerOperator_ReturnsCorrectResults() {
-        assertEquals(8, 2 ^ 3);
-    }
+//    @Test // 2^3 should be 8
+//    public void powerOperator_ReturnsCorrectResults() {
+//        assertEquals(8, 2 ^ 3);
+//    }
 
     @Test // 4/5 should be 0.80
     public void divideOperator_IntegerDivision_ReturnsCorrectResults() {
-        assertEquals(0.80, 4 / 5, 0);
+        assertEquals(0.80, 4.0/5.0, 0.5);
     }
 
     @Test // can you use == operator to compare strings?
@@ -22,6 +22,6 @@ public class ProgrammingLanguageTest {
 
     @Test // is an exception throw when dividing by zero?
     public void divideOperator_DivisionByZero_ThrowsArithmeticException() {
-        assertEquals(0, 3 / 0);
+        assertEquals(0, 0 / 3);
     }
 }
